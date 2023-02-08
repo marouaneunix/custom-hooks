f94478b17f6f411979ded7a9866a89aa43846ea3
Marouane EL MERROUNI <melmourrouni-externe@sigma.fr>
2023-02-08 07:15:22 +0100

    [TECH] publish the first version to npm

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,27 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/main.tsx'),
+      name: 'Custom Hooks',
+      fileName: (format) => `custom-hooks.${format}.js`
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React'
+        }
+      }
+    }
+  },
   plugins: [react()],
   test: {
     globals: true,
