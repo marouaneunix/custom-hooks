62363a1ac4d20a7015ef79ab9c3401ccfe44aba8
marouaneunix <elmerrouni.marouane@gmail.com>
2023-04-08 15:47:51 +0000

    [FEAT]: useLocationHash hook

---
+++ src/hooks/location-hash/index.ts
@@ -0,0 +1,31 @@
+import { useCallback, useEffect, useState } from "react";
+
+export const useLocationHash = () => {
+  const [hash, setHash] = useState<string>(() => window.location.hash);
+
+
+  useEffect(() => {
+    const hashChangeHandler = () => {
+      setHash(window.location.hash);
+    };
+
+    window.addEventListener("hashchange", hashChangeHandler);
+
+    return () => {
+      window.removeEventListener("hashchange", hashChangeHandler);
+    };
+  }, []);
+
+  const updateHash = useCallback(
+    (newHash: string) => {
+      if (newHash !== hash) {
+        window.location.hash = newHash;
+        setHash(newHash);
+      }
+    },
+    [hash]
+  );
+
+
+  return [hash, updateHash];
+};
---
+++ src/hooks/location-hash/useLocationHash.test.tsx
@@ -0,0 +1,47 @@
+import { fireEvent, render, screen } from "@testing-library/react"
+import { useLocationHash } from ".";
+import { ChangeEvent } from "react";
+
+
+
+const Target = () => {
+    const [hash, setHash] = useLocationHash();
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
+        setHash(e.target.value);
+    }    
+
+    return (
+        <div data-testid="target">
+            {hash}
+            <input value={hash} onChange={handleChange} aria-label="hash-input" />
+        </div>
+    )
+}
+
+const setup = () => {
+    const utils = render(<Target />)
+    const input = screen.getByLabelText('hash-input')
+    return {
+        input,
+        ...utils,
+    }
+}
+
+
+describe('useLocationHash hook', () => {
+    it('should get the window location hash', () => {
+        window.location.hash = '#my-hash';
+        setup();
+
+        expect(screen.getByTestId('target')).toHaveTextContent('my-hash');
+    })
+
+    it('should update the window location hash', () => {
+        window.location.hash = '';
+        const {input} = setup();
+
+        fireEvent.change(input, {target: {value: 'custom-hash'}})
+        expect(screen.getByTestId('target')).toHaveTextContent('custom-hash');
+    })
+})
